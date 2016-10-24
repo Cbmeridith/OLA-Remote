509457e8ffb6212f748dba4ee7ca66688d21be91
Cody Meridith <CodyM@codys-macbook-pro.local>
2016-10-24 12:20:44 -0500

    Initial Commit

---
+++ Pi-Mote/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pi-Mote
+//
+//  Created by Cody Meridith on 10/24/16.
+//  Copyright © 2016 Cody Meridith. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
