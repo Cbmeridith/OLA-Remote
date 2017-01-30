a30920d8fa1f0705582ba118a6937ae871cf5eca
Cody Meridith <CodyM@Codys-MacBook-Pro.local>
2017-01-29 18:43:17 -0600

    add test line

--- Pi-Mote/FirstViewController.swift
+++ Pi-Mote/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //test
         // Do any additional setup after loading the view, typically from a nib.
     }
 
