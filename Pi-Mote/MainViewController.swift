b0f4422cf42d98777e523358a819d6c13fcd428c
Cody Meridith <CodyM@Codys-MacBook-Pro.local>
2017-02-05 12:00:58 -0600

    Dynamic button base done

--- Pi-Mote/MainViewController.swift
+++ Pi-Mote/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  MainViewController.swift
 //  Pi-Mote
 //
 //  Created by Cody Meridith on 10/24/16.
@@ -13,36 +13,39 @@ class MainViewController: UIViewController {
     let pi = PiStream()
     var pathToSettings: String!
     var settings: NSMutableDictionary!
+    var buttons: [UIButton]!
     
+    //TODO: store color of light buttons in array for expandibility
     var lights = [false, false, false, false]
-    
-    @IBOutlet weak var buttonBlue: UIButton!
-    @IBOutlet weak var buttonBrightDown: UIButton!
-    @IBOutlet weak var buttonBrightUp: UIButton!
-    @IBOutlet weak var buttonCyan: UIButton!
-    @IBOutlet weak var buttonFlux: UIButton!
-    @IBOutlet weak var buttonGreen: UIButton!
-    @IBOutlet weak var buttonMovie: UIButton!
+
     @IBOutlet weak var buttonOff: UIButton!
     @IBOutlet weak var buttonOn: UIButton!
-    @IBOutlet weak var buttonOrange: UIButton!
-    @IBOutlet weak var buttonPink: UIButton!
-    @IBOutlet weak var buttonPurple: UIButton!
-    @IBOutlet weak var buttonRand: UIButton!
-    @IBOutlet weak var buttonRed: UIButton!
-    @IBOutlet weak var buttonSoftWhite: UIButton!
-    @IBOutlet weak var buttonSwap: UIButton!
-    @IBOutlet weak var buttonWhite: UIButton!
-    @IBOutlet weak var buttonYellow: UIButton!
+    
     @IBOutlet weak var buttonLight0: UIButton!
     @IBOutlet weak var buttonLight1: UIButton!
     @IBOutlet weak var buttonLight2: UIButton!
     @IBOutlet weak var buttonLight3: UIButton!
+                                            //Default Function:
+    @IBOutlet weak var button0: UIButton!   //Movie
+    @IBOutlet weak var button1: UIButton!   //Swap
+    @IBOutlet weak var button2: UIButton!   //Random
+    @IBOutlet weak var button3: UIButton!   //NightMode
+    @IBOutlet weak var button4: UIButton!   //Red
+    @IBOutlet weak var button5: UIButton!   //Green
+    @IBOutlet weak var button6: UIButton!   //Blue
+    @IBOutlet weak var button7: UIButton!   //SoftWhite
+    @IBOutlet weak var button8: UIButton!   //Orange
+    @IBOutlet weak var button9: UIButton!   //Purple
+    @IBOutlet weak var button10: UIButton!  //Cyan
+    @IBOutlet weak var button11: UIButton!  //BrightUp
+    @IBOutlet weak var button12: UIButton!  //Yellow
+    @IBOutlet weak var button13: UIButton!  //Pink
+    @IBOutlet weak var button14: UIButton!  //White
+    @IBOutlet weak var button15: UIButton!  //BrightDown
     
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        //TODO: Decide on color
         buttonLight0.backgroundColor = UIColor.lightGray;
         buttonLight1.backgroundColor = UIColor.lightGray;
         buttonLight2.backgroundColor = UIColor.lightGray;
@@ -53,8 +56,54 @@ class MainViewController: UIViewController {
         pathToSettings = Bundle.main.path(forResource: "Settings", ofType: "plist")
         settings = NSMutableDictionary(contentsOfFile: pathToSettings!)
         
+        let pathToButtons = Bundle.main.path(forResource: "Buttons", ofType: "plist")
+        let buttonCodes = NSDictionary(contentsOfFile: pathToButtons!)
+        
         pi.IP = settings?.value(forKey: "Address") as? String
         pi.openConnection()
+        
+        //put all buttons in array for easy referencing
+        buttons = [button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15]
+        
+        //pull RGB from assigned code for each button and set the background color of the button
+        for i in 0...buttons.count - 1 {
+            let currentCode = buttonCodes?.value(forKey: String(i)) as! String
+            //RGB codes are 9 characters in length, set button color to RGB code
+            //format: RRRGGGBBB
+            if currentCode.characters.count == 9 {
+                var startIndex = currentCode.startIndex
+                var endIndex = currentCode.index(startIndex, offsetBy: 3)
+                //no conversion from String -> CGFloat, must go String -> Float -> CGFloat
+                let r = CGFloat(Float(currentCode.substring(with: startIndex ..< endIndex))!)
+                
+                startIndex = endIndex
+                endIndex = currentCode.index(startIndex, offsetBy: 3)
+                let g = CGFloat(Float(currentCode.substring(with: startIndex ..< endIndex))!)
+                
+                startIndex = endIndex
+                endIndex = currentCode.index(startIndex, offsetBy: 3)
+                let b = CGFloat(Float(currentCode.substring(with: startIndex ..< endIndex))!)
+                
+                buttons[i].backgroundColor = UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+                
+                //print("\(r), \(g), \(b)")
+            }
+            //Code is a function, set button to corresponding image
+            else {
+                switch currentCode {
+                case "MOVIE": buttons[i].setImage(#imageLiteral(resourceName: "Movie"), for: .normal); break;
+                    case "SWAP": buttons[i].setImage(#imageLiteral(resourceName: "Swap"), for: .normal); break;
+                    case "RANDOM": buttons[i].setImage(#imageLiteral(resourceName: "Rand"), for: .normal); break;
+                    case "NIGHT": buttons[i].setImage(#imageLiteral(resourceName: "Flux"), for: .normal); break;
+                    case "BRIGHTUP": buttons[i].setImage(#imageLiteral(resourceName: "BrightUp"), for: .normal); break;
+                    case "BRIGHTDOWN": buttons[i].setImage(#imageLiteral(resourceName: "BrightDown"), for: .normal); break;
+                    default: print("Code Not Recognized!"); break;
+                }
+            }
+            
+        }
+        
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -67,6 +116,8 @@ class MainViewController: UIViewController {
     }
     
     func setLight(i: Int, on: Bool) {
+        //flips lights button color between on/off
+        //TODO: read in from light array in a loop, instead of hardcoding lights
         lights[i] = on
         if lights[i] {
             switch i {
@@ -84,6 +135,26 @@ class MainViewController: UIViewController {
                 case 2: buttonLight2.backgroundColor = UIColor.lightGray; break;
                 case 3: buttonLight3.backgroundColor = UIColor.lightGray; break;
                 default: break;
+            }
+        }
+        
+        //if a button is pressed other than all, flip all button accordingly
+        if i <= lights.count - 1 && !on {
+            buttonLight3.backgroundColor = UIColor.lightGray
+            lights[lights.count - 1] = false
+        }
+        else {
+            //if all lights are on, turn "All" button on
+            var allOn = true
+            for i in 0...lights.count - 2 {
+                if !lights[i] {
+                    allOn = false
+                    break
+                }
+            }
+            if allOn {
+                buttonLight3.backgroundColor = UIColor.gray
+                lights[lights.count - 1] = true
             }
         }
     }
@@ -114,7 +185,6 @@ class MainViewController: UIViewController {
         }
         
         if let decMode = Int(binaryMode, radix: 2) {
-            print(decMode)
             return "MODE\(decMode)"
         }
         return "ERROR"
@@ -122,55 +192,42 @@ class MainViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton)
     {
-        let pathToColors = Bundle.main.path(forResource: "Colors", ofType: "plist")
-        let colors = NSDictionary(contentsOfFile: pathToColors!)
+        let pathToButtons = Bundle.main.path(forResource: "Buttons", ofType: "plist")
+        let buttonCodes = NSDictionary(contentsOfFile: pathToButtons!)
         
         var code = ""
         switch sender {
-            
-            case buttonBlue: code = colors?.value(forKey: "Blue") as! String; break
-            case buttonBrightDown: code = "BRIGHTDOWN"; break
-            case buttonBrightUp: code = "BRIGHTUP"; break
-            case buttonCyan: code = colors?.value(forKey: "Cyan") as! String; break
-            case buttonFlux: code = "FLUX"; break
-            case buttonGreen: code = colors?.value(forKey: "Green") as! String; break
+            //ON/OFF
+            case buttonOff: code = "OFF"; break
+            case buttonOn: code = "ON"; break
+            //MODE
             case buttonLight0: setLight(i: 0, on: !lights[0]); code = calculateMode(); break
             case buttonLight1: setLight(i: 1, on: !lights[1]); code = calculateMode(); break
             case buttonLight2: setLight(i: 2, on: !lights[2]); code = calculateMode(); break
             case buttonLight3: setLight(i: 3, on: !lights[3]); flipAllModeButtons(); code = calculateMode(); break
-            case buttonMovie: code = "MOVIE"; break
-            case buttonOff: code = "OFF"; break
-            case buttonOn: code = "ON"; break
-            case buttonOrange: code = colors?.value(forKey: "Orange") as! String; break
-            case buttonPink: code = colors?.value(forKey: "Pink") as! String; break
-            case buttonPurple: code = colors?.value(forKey: "Purple") as! String; break
-            case buttonRand: code = "RAND"; break
-            case buttonRed: code = colors?.value(forKey: "Red") as! String; break
-            case buttonSoftWhite: code = colors?.value(forKey: "SoftWhite") as! String; break
-            case buttonSwap: code = "SWAP"; break
-            case buttonWhite: code = colors?.value(forKey: "White") as! String; break
-            case buttonYellow: code = colors?.value(forKey: "Yellow") as! String; break
-            
-            default: break
-            
-        }
-                
-        pi.sendCode(code: code, length: code.characters.count)
-    }
-/*
-    @IBAction func selectedSegmentChanged(_ sender: UISegmentedControl) {
-        
-        var code = ""
-        switch sender.selectedSegmentIndex {
-            case 0: code = "MODE1"; break;
-            case 1: code = "MODE2"; break;
-            case 2: code = "MODE3"; break;
+            //DYNAMIC BUTTONS
+            case button0: code = buttonCodes?.value(forKey: "0") as! String; break
+            case button1: code = buttonCodes?.value(forKey: "1") as! String; break
+            case button2: code = buttonCodes?.value(forKey: "2") as! String; break
+            case button3: code = buttonCodes?.value(forKey: "3") as! String; break
+            case button4: code = buttonCodes?.value(forKey: "4") as! String; break
+            case button5: code = buttonCodes?.value(forKey: "5") as! String; break
+            case button6: code = buttonCodes?.value(forKey: "6") as! String; break
+            case button7: code = buttonCodes?.value(forKey: "7") as! String; break
+            case button8: code = buttonCodes?.value(forKey: "8") as! String; break
+            case button9: code = buttonCodes?.value(forKey: "9") as! String; break
+            case button10: code = buttonCodes?.value(forKey: "10") as! String; break
+            case button11: code = buttonCodes?.value(forKey: "11") as! String; break
+            case button12: code = buttonCodes?.value(forKey: "12") as! String; break
+            case button13: code = buttonCodes?.value(forKey: "13") as! String; break
+            case button14: code = buttonCodes?.value(forKey: "14") as! String; break
+            case button15: code = buttonCodes?.value(forKey: "15") as! String; break
             default: break;
         }
         
+        //print(code)
         pi.sendCode(code: code, length: code.characters.count)
     }
-*/
 }
  
 
